24e2015fe2663667d05d668ca366abc8bc3af948
Thibaud Colas <thibaudcolas@gmail.com>
2019-07-16 11:47:46 +0100

    Update snapshots with Airbnb config base

--- src/semver.test.js
+++ src/semver.test.js
@@ -12,9 +12,12 @@ describe('semver - should those tests break, consider releasing a new major vers
     it('has stable peerDependencies', () => {
         expect(pkg.peerDependencies).toMatchInlineSnapshot(`
             Object {
-              "eslint": "^6.0.0",
+              "eslint": "^5.0.0",
+              "eslint-config-airbnb": "^17.1.0",
               "eslint-config-prettier": "^6.0.0",
-              "eslint-plugin-react": "^7.14.2",
+              "eslint-plugin-import": "^2.18.0",
+              "eslint-plugin-jsx-a11y": "^6.2.0",
+              "eslint-plugin-react": "^7.14.0",
             }
         `);
     });
@@ -25,6 +28,6 @@ describe('semver - should those tests break, consider releasing a new major vers
         computed.extends = computed.extends.map((extend) =>
             extend.replace(/.*node_modules\//, ''),
         );
-        expect().toMatchSnapshot();
+        expect(computed).toMatchSnapshot();
     });
 });
