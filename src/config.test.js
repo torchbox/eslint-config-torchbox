956b45a9cefeeff5554a050b5fa1acfdf15cc8c7
Thibaud Colas <thibaudcolas@gmail.com>
2019-10-08 11:11:37 +0100

    Add test for React detection warning

--- src/config.test.js
+++ src/config.test.js
@@ -4,6 +4,8 @@ const { CLIEngine } = require('eslint');
 
 describe('config', () => {
     it('works', () => {
+        console.error = jest.fn();
+
         const cli = new CLIEngine({
             useEslintrc: false,
             configFile: path.join(__dirname, '..', 'config.js'),
@@ -13,5 +15,10 @@ describe('config', () => {
             errorCount: 2,
             warningCount: 0,
         });
+
+        // This project does not depend on React, so using the config will log an error.
+        expect(console.error).toHaveBeenCalledWith(
+            'Warning: React version was set to "detect" in eslint-plugin-react settings, but the "react" package is not installed. Assuming latest React version for linting.',
+        );
     });
 });
